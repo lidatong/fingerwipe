771845d9a1d24d8057afa4e27c09e4709d84f8c9
cli <charles.dt.li@gmail.com>
2015-11-14 14:49:50 -0500

    adding ui elements, extension icon

--- src/bg/background.js
+++ src/bg/background.js
@@ -8,52 +8,53 @@
 //example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
+    chrome.pageAction.show(sender.tab.id);
     sendResponse();
   });
-  
-  
+
+
 //Header override
 //Header override from http://stackoverflow.com/a/27936481/582136
 chrome.webRequest.onBeforeSendHeaders.addListener(
-        function(details) {
-          for (var i = 0; i < details.requestHeaders.length; ++i) {
-            if (details.requestHeaders[i].name === 'User-Agent') {
-				newUserAgent = updateUserAgent(details.requestHeaders[i].value)
-				if(newUserAgent) //update to new user agent
-					details.requestHeaders[i].value = newUserAgent
-				else //Delete user agent entirely
-					details.requestHeaders.splice(i,1)
-				break;
-            }
-          }
-          return {requestHeaders: details.requestHeaders};
-        },
-        {urls: ["<all_urls>"]},
-        ["blocking", "requestHeaders"]);
-		
-	
+  function(details) {
+    for (var i = 0; i < details.requestHeaders.length; ++i) {
+      if (details.requestHeaders[i].name === 'User-Agent') {
+        newUserAgent = updateUserAgent(details.requestHeaders[i].value)
+        if (newUserAgent) //update to new user agent
+          details.requestHeaders[i].value = newUserAgent
+        else //Delete user agent entirely
+          details.requestHeaders.splice(i, 1)
+        break;
+      }
+    }
+    return {
+      requestHeaders: details.requestHeaders
+    };
+  }, {
+    urls: ["<all_urls>"]
+  }, ["blocking", "requestHeaders"]);
 
-	
-userAgentParse = function(userAgent){
-	var userAgentRegex = /([^ ]*)( \([^\)]*\))? ?/g
-	var userAgentComponents = []
-	var prevIndex = -1
-	while (userAgentRegex.lastIndex != prevIndex) {
-		prevIndex = userAgentRegex.lastIndex
-		match = userAgentRegex.exec(userAgent)
-		userAgentComponents.push(match[0])
-		msg = 'Match : ' + match[0] + '\nNext match starts at ' + userAgentRegex.lastIndex;
-		console.log(msg);
-	}
-	return userAgentComponents
+
+
+userAgentParse = function(userAgent) {
+  var userAgentRegex = /([^ ]*)( \([^\)]*\))? ?/g
+  var userAgentComponents = []
+  var prevIndex = -1
+  while (userAgentRegex.lastIndex != prevIndex) {
+    prevIndex = userAgentRegex.lastIndex
+    match = userAgentRegex.exec(userAgent)
+    userAgentComponents.push(match[0])
+    msg = 'Match : ' + match[0] + '\nNext match starts at ' + userAgentRegex.lastIndex;
+    console.log(msg);
+  }
+  return userAgentComponents
 }
-		
-updateUserAgent = function(userAgent){
-	console.log("Old user agent: " + userAgent)
-	var components = userAgentParse(userAgent)
-	var newUA = components[0] + components[1]
-	console.log("New user agent: " + newUA)
-	//return newUA
-	return userAgent
+
+updateUserAgent = function(userAgent) {
+  console.log("Old user agent: " + userAgent)
+  var components = userAgentParse(userAgent)
+  var newUA = components[0] + components[1]
+  console.log("New user agent: " + newUA)
+    //return newUA
+  return userAgent
 }
